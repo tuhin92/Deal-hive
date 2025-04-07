eb3ed50780060633a113da766551dca9375e065f
tuhin92 <tuhinalmamun71m@gmail.com>
2025-04-07 09:44:51 +0600

    added shop items categories

--- lib/Views/app_home_screen.dart
+++ lib/Views/app_home_screen.dart
@@ -10,6 +10,51 @@ class AppHomeScreen extends StatefulWidget {
 }
 
 class _AppHomeScreenState extends State<AppHomeScreen> {
+  Widget _buildCategoryItem(String imageUrl, String categoryName) {
+    return Container(
+      width: 90,
+      margin: EdgeInsets.only(right: 12),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            imageUrl,
+            height: 40,
+            width: 40,
+            fit: BoxFit.contain,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(
+                child: CircularProgressIndicator(
+                  value:
+                      loadingProgress.expectedTotalBytes != null
+                          ? loadingProgress.cumulativeBytesLoaded /
+                              loadingProgress.expectedTotalBytes!
+                          : null,
+                ),
+              );
+            },
+            errorBuilder: (context, error, stackTrace) {
+              return Icon(Icons.error, size: 40);
+            },
+          ),
+          SizedBox(height: 8),
+          Text(
+            categoryName,
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            textAlign: TextAlign.center,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,20 +120,56 @@ class _AppHomeScreenState extends State<AppHomeScreen> {
                   ),
                   Text(
                     "See All",
-                    style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 16,
-                    ),
+                    style: TextStyle(color: Colors.black45, fontSize: 16),
                   ),
                 ],
               ),
             ),
 
             // category items part
-
-
-
-
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Container(
+                height: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/3194/3194766.png",
+                      "Fruits",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/3075/3075975.png",
+                      "Fish",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/3004/3004458.png",
+                      "Medicine",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/1005/1005665.png",
+                      "Beauty",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/3659/3659899.png",
+                      "Electronics",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/2331/2331966.png",
+                      "Clothing",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/1830/1830847.png",
+                      "Home & Kitchen",
+                    ),
+                    _buildCategoryItem(
+                      "https://cdn-icons-png.flaticon.com/512/857/857418.png",
+                      "Sports & Outdoors",
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
       ),
