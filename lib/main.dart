ccabb7dd44ee10b885147d8e5aef6c882d518110
tuhin92 <tuhinalmamun71m@gmail.com>
2025-04-15 23:21:34 +0600

    initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:application/Views/app_main_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
